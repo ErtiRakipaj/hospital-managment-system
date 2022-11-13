ca47a247a3a483c7986d635d71f29f6f515ee413
ErtiDesu <erti.rakipaj@gmail.com>
2022-11-13 11:15:56 +0100

    changed connection db file

--- src/main/java/org/example/hospitalmanagmentsystem/connection/Connection.java
+++
@@ -1,4 +0,0 @@
-package org.example.hospitalmanagmentsystem.connection;
-
-public class Connection {
-}
---
+++ src/main/java/org/example/hospitalmanagmentsystem/connection/ConnectionDb.java
@@ -0,0 +1,10 @@
+package org.example.hospitalmanagmentsystem.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDb {
+
+
+}
